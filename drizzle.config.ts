b4bbc8bbad38fdb7df66fe5bee80bd198c48f3ac
Thanukamax <159769064+Thanukamax@users.noreply.github.com>
2026-01-08 22:04:56 +0530

    feat: use env var for D1 database ID, add .env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,7 @@ export default drizzleD1Config(
   {
     accountId: process.env.CLOUDFLARE_D1_ACCOUNT_ID,
     apiToken: process.env.CLOUDFLARE_D1_API_TOKEN,
-    databaseId: '45b02f1a-9d64-48b1-bd7a-9a78999d1f66',
+    databaseId: process.env.CLOUDFLARE_D1_DATABASE_ID,
     binding: 'DB',
     remote: process.env.REMOTE === 'true' || process.env.REMOTE === '1',
   }
